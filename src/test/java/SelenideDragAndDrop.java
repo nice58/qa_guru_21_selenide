a22d5c5ec24394bdee9438fca49dafca65a87aa8
n.trubanenko <n.trubanenko@evotor.ru>
2023-08-15 11:37:37 +0300

    selenide test correction

--- src/test/java/SelenideDragAndDrop.java
+++ src/test/java/SelenideDragAndDrop.java
@@ -1,4 +1,5 @@
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
@@ -6,14 +7,15 @@ import static com.codeborne.selenide.DragAndDropOptions.to;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SelenideDragAndDrop {
-
-    @Test
-    void selenideDragAndDropTest(){
-
+    @BeforeAll
+    static void beforeAll() {
         open("https://the-internet.herokuapp.com/drag_and_drop");
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
+    }
 
+    @Test
+    void selenideDragAndDropTest(){
         $("#column-a").shouldHave(text("A"));
         $("#column-b").shouldHave(text("B"));
         $("#column-a").dragAndDrop(to($("#column-b")));
